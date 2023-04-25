c386429f5d0c205fdff9b4a3368db3ca03b0bdd4
Moh88CS <mohamed.abdulal88@gmail.com>
2023-04-25 03:54:23 -0400

    Uses correct imports and displays messeges

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -2,6 +2,39 @@
 
 // bin/rps-cli.js
 import { rps } from '../lib/rpsls.js';
-import { processArgs } from './cli-helper.js';
+import minimist from 'minimist';
 
-processArgs(rps, 'node-rps', ['rock', 'paper', 'scissors']);
+const args = minimist(process.argv.slice(2));
+const validChoices = ['rock', 'paper', 'scissors'];
+processArgs(rps, 'node-rps', validChoices, args);
+
+function processArgs(gameFunction, commandName, validChoices, args) {
+  if (args.h || args.help) {
+    displayHelp(commandName);
+    process.exit(0);
+  }
+
+  if (args.r || args.rules) {
+    displayRules();
+    process.exit(0);
+  }
+
+  try {
+    const playerChoice = args._[0] || gameFunction();
+    const gameResult = gameFunction(playerChoice);
+    console.log(JSON.stringify(gameResult));
+  } catch (error) {
+    console.error(error.message);
+    displayHelp(commandName);
+    displayRules();
+    process.exit(1);
+  }
+}
+
+function displayHelp(commandName) {
+  console.log(`Usage: ${commandName} [SHOT]\nPlay Rock Paper Scissors (RPS)\n\n  -h, --help      display this help message and exit\n  -r, --rules     display the rules and exit`);
+}
+
+function displayRules() {
+  console.log(`Rules for Rock Paper Scissors:\n\n  - Scissors CUTS Paper\n  - Paper COVERS Rock\n  - Rock CRUSHES Scissors`);
+}
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -2,6 +2,50 @@
 
 // bin/rpsls-cli.js
 import { rpsls } from '../lib/rpsls.js';
-import { processArgs } from './cli-helper.js';
+import minimist from 'minimist';
 
-processArgs(rpsls, 'node-rpsls', ['rock', 'paper', 'scissors', 'lizard', 'spock']);
+const args = minimist(process.argv.slice(2));
+const validChoices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+processArgs(rpsls, 'node-rpsls', validChoices, args);
+
+function processArgs(gameFunction, commandName, validChoices, args) {
+  if (args.h || args.help) {
+    displayHelp(commandName);
+    process.exit(0);
+  }
+
+  if (args.r || args.rules) {
+    displayRules();
+    process.exit(0);
+  }
+
+  try {
+    const playerChoice = args._[0] || gameFunction();
+    const gameResult = gameFunction(playerChoice);
+    console.log(JSON.stringify(gameResult));
+  } catch (error) {
+    console.error(error.message);
+    displayHelp(commandName);
+    displayRules();
+    process.exit(1);
+  }
+}
+
+function displayHelp(commandName) {
+  console.log(`Usage: ${commandName} [SHOT]\nPlay the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!\n\n  -h, --help        display this help message and exit\n  -r, --rules       display the rules and exit`);
+}
+
+function displayRules() {
+  console.log(`Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
+
+  - Scissors CUTS Paper
+  - Paper COVERS Rock
+  - Rock SMOOSHES Lizard
+  - Lizard POISONS Spock
+  - Spock SMASHES Scissors
+  - Scissors DECAPITATES Lizard
+  - Lizard EATS Paper
+  - Paper DISPROVES Spock
+  - Spock VAPORIZES Rock
+  - Rock CRUSHES Scissors`) 
+}
